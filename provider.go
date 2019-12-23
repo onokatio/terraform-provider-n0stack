bfaf35f06e9e9750a7da4eb945206c5dd08a1fb1
onokatio <onokatio@gmail.com>
2019-12-23 13:00:04 +0900

    use resource_n0stack_blockstorage

--- provider.go
+++ provider.go
@@ -6,6 +6,9 @@ import (
 
 func Provider() *schema.Provider {
         return &schema.Provider{
-                ResourcesMap: map[string]*schema.Resource{},
+		Schema: map[string]*schema.Schema{},
+                ResourcesMap: map[string]*schema.Resource{
+			"n0stack_blockstorage": resource_n0stack_blockstorage(),
+		},
         }
 }
