88e7581d7bd658518a340c2a6f9ea73f33d7f2af
onokatio <onokatio@gmail.com>
2019-12-24 00:44:48 +0900

    force refresh

--- test.tf
+++ test.tf
@@ -7,6 +7,7 @@ resource "n0stack_blockstorage" "example" {
 	annotations = {
 		annotation1 = "hoge"
 		"n0core/provisioning/block_storage/request_node_name" = "archlinux"
+		"n0core/provisioning/block_storage/url"               = "file:///var/lib/n0core/block_storage/test_storage"
 	}
 	labels = {
 		label1 = "hoge"
