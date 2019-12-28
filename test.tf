a43597e5b124fcf7f61782e25c23c508e4341e7d
onokatio <onokatio@gmail.com>
2019-12-28 15:16:49 +0900

    use dc n0stack for test

--- test.tf
+++ test.tf
@@ -1,68 +1,39 @@
 provider "n0stack" {
-	endpoint = "192.168.1.31:20180"
+	endpoint = "172.16.14.10:20180"
 }
 
-resource "n0stack_blockstorage" "FetchUbuntuDisk" {
-	blockstorage_name = "ubuntu-bionic"
-	annotations = {
-		"n0core/provisioning/block_storage/request_node_name" = "archlinux"
-	}
-	labels = {
-		label1 = "hoge"
-	}
-	request_bytes = 1073741824
-	limit_bytes = 10737418240
-	source_url = "http://localhost/bionic-server-cloudimg-amd64.img"
-}
-
-resource "n0stack_image" "RegisterUbuntuAsImageLatest" {
-	image_name = "ubuntu-bionic-image"
-	tags = ["latest"]
-	blockstorage_name = "ubuntu-bionic"
-
-	depends_on = [n0stack_blockstorage.FetchUbuntuDisk]
-}
-
-
-resource "n0stack_blockstorage" "CreateNewDiskFromImage" {
-	image_name = "ubuntu-bionic-image"
+resource "n0stack_blockstorage" "MyDiskFromImage" {
+	image_name = "baseimage-ubuntu"
 	tag = "latest"
-	blockstorage_name = "test-disk"
+	blockstorage_name = "MyDisk"
 	annotations = {
-		"n0core/provisioning/block_storage/request_node_name" = "archlinux"
+		"n0core/provisioning/block_storage/request_node_name" = "n0node00"
 	}
 	request_bytes = 1073741824
 	limit_bytes = 10737418240
-
-	depends_on = [n0stack_image.RegisterUbuntuAsImageLatest]
 }
 
-resource "n0stack_network" "CreateNetwork" {
-	name = "test-network"
+resource "n0stack_network" "MyNetwork" {
+	name = "MyNetwork"
 	ipv4_cidr = "192.168.100.0/24"
 	annotations = {
-		"n0core/provisioning/block_storage/request_node_name" = "archlinux"
+		"n0core/provisioning/virtual_machine/vlan_id" = "126"
 	}
 }
 
 resource "n0stack_virtualmachine" "CreateVM" {
 	name = "test-vm"
 	annotations = {
-		"n0core/provisioning/virtual_machine/request_node_name" = "archlinux"
+		"n0core/provisioning/virtual_machine/request_node_name" = "n0node00"
 	}
 	request_cpu_milli_core = 10
 	limit_cpu_milli_core = 1000
 	request_memory_bytes = 536870912
 	limit_memory_bytes = 536870912
-	block_storage_names = ["test-disk"]
+	block_storage_names = [n0stack_blockstorage.MyDiskFromImage.blockstorage_name]
 
 	nics {
-		network_name = "test-network"
+		network_name = n0stack_network.MyNetwork.name
 		ipv4_address = "192.168.100.1"
 	}
-
-	depends_on = [
-		n0stack_blockstorage.CreateNewDiskFromImage,
-		n0stack_network.CreateNetwork,
-	]
 }
