f43638b096e26d6cf83f9d8f39b92b378bb39f7d
onokatio <onokatio@gmail.com>
2020-03-09 00:03:43 +0900

    fix

--- crosscompile.sh
+++ crosscompile.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 
 GOARCH=amd64
-
 GOOS=darwin
 go build -o terraform-provider-n0stack-$GOOS-$GOARCH
 
+GOARCH=amd64
 GOOS=linux
 go build -o terraform-provider-n0stack-$GOOS-$GOARCH
 
-GOOS=windows
+GOARCH=386
+GOOS=linux
 go build -o terraform-provider-n0stack-$GOOS-$GOARCH
+
+GOARCH=amd64
+GOOS=windows
+go build -o terraform-provider-n0stack-$GOOS-$GOARCH.exe
+
+GOARCH=386
+GOOS=windows
+go build -o terraform-provider-n0stack-$GOOS-$GOARCH.exe
